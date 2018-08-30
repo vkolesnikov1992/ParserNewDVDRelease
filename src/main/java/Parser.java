67b78d6854b89d2c6df334a545a5ba5b3121576e
MrGoodwin <Xr5pk47ttk>
2018-08-30 22:46:09 +0900

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,28 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Parser {
+
+    private static Document getPage() throws IOException {
+    String url = "https://www.kinopoisk.ru/comingsoon/dvd/";
+    Document page = Jsoup.parse(new URL(url), 3000);
+    return page;
+
+   }
+
+    public static void main(String[] args) throws IOException {
+        Document page = getPage();
+        Element tableKinopoisk = page.select("table[class=filmList]").first();
+        Elements names = tableKinopoisk.select("tr");
+        System.out.println(names.select("td[class=news]").text());
+        System.out.println(names.select("td[valign=top]").text());
+
+
+
+    }
+}
