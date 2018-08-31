caf194c806d83d0dc1007ce69fad92ff7a9c9adc
vkolesnikov <__kolesnikov__@mail.ru>
2018-08-31 13:47:46 +0900

    commit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,19 +9,24 @@ import java.net.URL;
 public class Parser {
 
     private static Document getPage() throws IOException {
-    String url = "https://www.kinopoisk.ru/comingsoon/dvd/";
-    Document page = Jsoup.parse(new URL(url), 3000);
-    return page;
+        String url = "https://www.kinopoisk.ru/comingsoon/dvd/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        return page;
 
-   }
+    }
 
     public static void main(String[] args) throws IOException {
         Document page = getPage();
         Element tableKinopoisk = page.select("table[class=filmList]").first();
         Elements names = tableKinopoisk.select("tr");
         System.out.println(names.select("td[class=news]").text());
-        System.out.println(names.select("td[valign=top]").text());
 
+        for (Element name : names) {
+            String films = name.select("td[valign=top]").text();
+            if (!films.isEmpty()) {
+                System.out.println(films);
+            }
+        }
 
 
     }
